6ac9d742fd457d3daf8b4da119bc28a706a6d164
hafi-shah <devhafi1337@gmail.com>
2023-05-20 03:48:11 +0500

    adding widgets in app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { CompaniesComponent } from './modules/companies/companies.component';
+import { FeedComponent } from './modules/feed/feed.component';
+import { MyProfileComponent } from './modules/my-profile/my-profile.component';
+
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  {path : '' , component: HomeComponent},
+  { path: 'home', component: HomeComponent },
+  {path: 'companies', component: CompaniesComponent},
+  {path: 'feed', component: FeedComponent},
+  {path: 'myProfile', component: MyProfileComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './modules/home/home.component';
+import { CompaniesComponent } from './modules/companies/companies.component';
+import { FeedComponent } from './modules/feed/feed.component';
+import { MyProfileComponent } from './modules/my-profile/my-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CompaniesComponent,
+    FeedComponent,
+    MyProfileComponent
   ],
   imports: [
     BrowserModule,
